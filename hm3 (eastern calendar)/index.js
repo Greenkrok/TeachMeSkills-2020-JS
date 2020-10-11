22020891569654ee5e0188b2ed59ea7be903db2e
Greenkrok <hellsing_13@mail.ru>
2020-10-11 17:30:56 +0300

    eastern calendar

---
+++ hm3 (eastern calendar)/index.js
@@ -0,0 +1,43 @@
+let a = +(prompt("Введите год 1936-1947"));
+
+switch (a) {
+    case 1936:
+        alert ("Крыса");
+        break;
+    case 1937:
+        alert ("Бык");
+        break;
+    case 1938:
+        alert ("Тигр");
+        break;
+    case 1939:
+        alert ("Кот");
+        break;
+    case 1940:
+        alert ("Дракон");
+        break;
+    case 1941:
+        alert ("Змея");
+        break;
+    case 1942:
+        alert ("Лошадь");
+        break;
+    case 1943:
+        alert ("Коза");
+        break;
+    case 1944:
+        alert ("Обезьяна");
+        break;
+    case 1945:
+        alert ("Петух");
+        break;
+    case 1946:
+        alert ("Собака");
+        break;
+    case 1947:
+        alert ("Кобан");
+        break;
+    default:
+        alert ("Недопустимый формат");
+        break;
+}
