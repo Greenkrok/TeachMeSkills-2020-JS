8b88ab930cde57ae22fcf541d93da1663e710aac
Greenkrok <hellsing_13@mail.ru>
2020-10-14 20:33:18 +0300

    feat: add task 2

--- hm4/script.js
+++ hm4/script.js
@@ -1,18 +1,30 @@
 // TASK-1
-let a = +prompt('Введите число');
+// let a = +prompt('Введите число');
 
-if(isNaN(a)) {
-    alert('Это не число');
-}
+// if(isNaN(a)) {
+//     alert('Это не число');
+// }
 
-else if(a > 0) {
-    alert('Число положительное');
-}
+// else if(a > 0) {
+//     alert('Число положительное');
+// }
 
-else if(a < 0) {
-    alert('Число отрицательное');
-}
+// else if(a < 0) {
+//     alert('Число отрицательное');
+// }
 
-else {
-    alert('Число 0');
+// else {
+//     alert('Число 0');
+// }
+
+// TASK-2
+let userYear = +prompt("Введите свой возраст");
+
+switch(true) {
+    case (userYear <= 0 || userYear > 120 || isNaN(userYear)):
+        alert('Кто-то врот');
+        break;
+    default:
+        alert('Нормас');
+        break;
 }
