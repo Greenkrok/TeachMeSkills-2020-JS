97c5219d18e7a9c2e41c96d551c46eca77602233
Greenkrok <hellsing_13@mail.ru>
2020-10-16 14:28:53 +0300

    feat: add task 4

--- hm4/script.js
+++ hm4/script.js
@@ -30,11 +30,42 @@
 // }
 
 // TASK-3 (Запросить у пользователя число и вывести его модуль (|7| = 7, |-7| = 7))
-let num = process.argv[2];
-if(isNaN(num)) {
-    console.log('Это не число');
+// let num = process.argv[2];
+// if(isNaN(num)) {
+//     console.log('Это не число');
+// }
+
+// else {
+//     console.log(`|${num}| = `,Math.abs(num));
+// }
+
+// TASK-4 (Запросить у пользователя время (часы, минуты, секунды) и проверить корректность введенных данных)
+let hours = process.argv[2];
+
+if(hours < 0 || hours >24 || isNaN(hours)) {
+    console.log('Столько часов на часах не бывает');
 }
 
 else {
-    console.log(`|${num}| = `,Math.abs(num));
+    console.log(hours + ' часов');
+}
+
+let minutes = process.argv[3];
+
+if(minutes < 0 || minutes >59 || isNaN(minutes)) {
+    console.log('Столько минут на часах не бывает');
+}
+
+else {
+    console.log(minutes + ' минут');
+}
+
+let seconds = process.argv[4];
+
+if(seconds < 0 || seconds >59 || isNaN(seconds)) {
+    console.log('Столько секунд на часах не бывает');
+}
+
+else {
+    console.log(seconds + ' секунд');
 }
