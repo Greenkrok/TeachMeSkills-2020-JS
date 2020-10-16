fae385a56a5f1720c64c6d06bf351f4ff1b6cf2e
Greenkrok <hellsing_13@mail.ru>
2020-10-16 13:41:56 +0300

    feat: add task 3

--- hm4/script.js
+++ hm4/script.js
@@ -1,4 +1,4 @@
-// TASK-1
+// TASK-1 (Запросить у пользователя число и определить, оно положительное, отрицательное или ноль)
 // let a = +prompt('Введите число');
 
 // if(isNaN(a)) {
@@ -17,14 +17,24 @@
 //     alert('Число 0');
 // }
 
-// TASK-2
-let userYear = +prompt("Введите свой возраст");
+// TASK-2 (Запросить у пользователя его возраст и проверить корректность введенных данных (0–120 лет))
+// let userYear = +prompt("Введите свой возраст");
 
-switch(true) {
-    case (userYear <= 0 || userYear > 120 || isNaN(userYear)):
-        alert('Кто-то врот');
-        break;
-    default:
-        alert('Нормас');
-        break;
+// switch(true) {
+//     case (userYear <= 0 || userYear > 120 || isNaN(userYear)):
+//         alert('Кто-то врот');
+//         break;
+//     default:
+//         alert('Нормас');
+//         break;
+// }
+
+// TASK-3 (Запросить у пользователя число и вывести его модуль (|7| = 7, |-7| = 7))
+let num = process.argv[2];
+if(isNaN(num)) {
+    console.log('Это не число');
+}
+
+else {
+    console.log(`|${num}| = `,Math.abs(num));
 }
